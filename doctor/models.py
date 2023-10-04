961d449a665e7f102fa50137892d89b9d9c63228
ogabek <quvvatullayev@gmail.com>
2023-10-04 14:03:10 +0500

    doctor and commint are created

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Doctor(AbstractUser):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    phone = models.CharField(max_length=30)
+    job_time_start = models.TimeField()
+    job_time_fenish = models.TimeField()
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Commint(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    customer = models.ForeignKey()
+    description = models.TextField()
