6271bd86276fc9c67e79db7ccaaca1633d789283
ogabek <quvvatullayev@gmail.com>
2023-10-04 14:15:23 +0500

    fix models

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class Customer(AbstractBaseUser):
+    name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=30)
+    
+    def __str__(self) -> str:
+        return self.name
--- doctor/models.py
+++ doctor/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from customer.models import Customer
 
-class Doctor(AbstractUser):
+class Doctor(AbstractBaseUser):
     name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
     description = models.TextField()
     phone = models.CharField(max_length=30)
     job_time_start = models.TimeField()
@@ -13,5 +15,5 @@ class Doctor(AbstractUser):
     
 class Commint(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    customer = models.ForeignKey()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     description = models.TextField()
