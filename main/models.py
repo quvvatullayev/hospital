e865c87f66526cb6e303e0b03dadc99e97aa5127
ogabek <quvvatullayev@gmail.com>
2023-10-04 15:54:09 +0500

    hospital is create model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Hospital(models.Model):
+    name = models.CharField(max_length=50)
+    img = models.ImageField('./hospital_imgs')
+    description = models.TextField()
+    location = models.CharField(max_length=225)
+    phone = models.CharField(max_length=30)
+    start_time = models.TimeField()
+    finesh_time = models.TimeField()
+
+    def __str__(self) -> str:
+        return self.name
