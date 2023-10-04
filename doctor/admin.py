99ce013879d2246ca8709bdfe679455fbf182132
ogabek <quvvatullayev@gmail.com>
2023-10-04 14:25:38 +0500

    models are add site

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Commint, Doctor, Category
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.register([Commint, Doctor, Category])
+admin.site.unregister([Group])
