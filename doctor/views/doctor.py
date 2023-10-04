cb50187b77a8fe4a8f007158d53b8451e2d3305d
ogabek <quvvatullayev@gmail.com>
2023-10-04 15:29:43 +0500

    doctor list create view is add

---
+++ doctor/views/doctor.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from ..models import Doctor
+from ..serializers import DoctorSerializers
+
+class DoctorListCreateView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializers
+    
