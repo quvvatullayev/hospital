81f471f8e97db6bbf35906aad332bf8fe4d5c094
ogabek <quvvatullayev@gmail.com>
2023-10-04 16:09:29 +0500

    hospitall list creawte view is created

---
+++ main/views/hospital.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from ..serializers import HospitalSerializers, Hospital
+
+class HospitalListCreateView(generics.ListCreateAPIView):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializers
