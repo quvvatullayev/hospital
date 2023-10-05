d33d7d51035079eb512ba8dc8dc3b6a234a19126
ogabek <quvvatullayev@gmail.com>
2023-10-05 14:43:43 +0500

    order views are add urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views.customer import CustomerListCreateView, CustomerRetrieveUpdateDestroyView
+from .views.order import OrderListCreateView, OrderRetrieveUpdateDestroyView
 
 urlpatterns = [
     # Customer views
     path('list/', CustomerListCreateView.as_view()),
     path('detil/<int:pk>/', CustomerRetrieveUpdateDestroyView.as_view()),
+    # Order views
+    path('order/lsit/', OrderListCreateView.as_view()),
+    path('order/detil/<int:pk>/', OrderRetrieveUpdateDestroyView.as_view())
 ]
