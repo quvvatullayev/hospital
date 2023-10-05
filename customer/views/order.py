1bb2cb5ce63672c34098995fdeb4187a4a769dd2
ogabek <quvvatullayev@gmail.com>
2023-10-05 14:39:52 +0500

    order list create view is created

---
+++ customer/views/order.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from ..models import Order
+from ..serializers import OrderSerializer
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    
