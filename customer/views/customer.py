c1c2d84820b47c3cb0e5d2d86d0bacbe086fe1c0
ogabek <quvvatullayev@gmail.com>
2023-10-05 14:07:16 +0500

    customer list create view is created

--- customer/views/customer.py
+++ customer/views/customer.py
@@ -2,3 +2,9 @@ from rest_framework import generics
 from rest_framework.request import Request
 from rest_framework.response import Response
 from ..models import CustomerModel, Order
+from ..serializers import CustomerModelSerializer
+
+class CustomerListCreateView(generics.ListCreateAPIView):
+    queryset = CustomerModel.objects.all()
+    serializer_class = CustomerModelSerializer
+    
