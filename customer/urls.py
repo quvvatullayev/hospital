a501d775de62642067ead2e16fded18cfe874466
ogabek <quvvatullayev@gmail.com>
2023-10-05 14:44:45 +0500

    fix

--- customer/urls.py
+++ customer/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('list/', CustomerListCreateView.as_view()),
     path('detil/<int:pk>/', CustomerRetrieveUpdateDestroyView.as_view()),
     # Order views
-    path('order/lsit/', OrderListCreateView.as_view()),
+    path('order/list/', OrderListCreateView.as_view()),
     path('order/detil/<int:pk>/', OrderRetrieveUpdateDestroyView.as_view())
 ]
